338b75ffee5f25806dab030f449a0c3493329d72
PritamDas <pritam.das.santu@gmail.com>
2024-05-03 21:13:44 +0530

    added .gitgnore and some comments

--- second/src/main.rs
+++ second/src/main.rs
@@ -1,11 +1,9 @@
 fn main() {
     
-    Ownership Example
+    // Ownership Example
 
     let num1: String = String::from("Rihanna");
     let _num2: String = num1.clone();
 
     println!("{}", num1);
-
-
 }
